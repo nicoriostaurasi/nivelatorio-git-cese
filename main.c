9a1a9652d5f0a9e0a99bea4fab5bc0a5530b11b4
Nicolas Rios <nrios@pdssa.com.ar>
2026-03-04 22:23:52 -0300

    creada 2da version

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello, World!\n");
+    int a = 4;
+    int b = 5;
+    printf("The sum of %d and %d is %d\n", a, b, a + b);
+    return 0;
+}
---
+++ test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main_test() {
+    printf("Hello, test World!\n");
+    return 0;
+}
