dc3fe771f8654dece7e4e337470f4049a0a3d3e7
Nicolas Rios <nrios@pdssa.com.ar>
2026-03-04 22:32:28 -0300

    6ta version

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Agrego comentarios independientes para el 3er commit
+// Agrego comentarios para generar problemas de mergeo
 int main() {
     printf("Hello, World!\n");
     int a = 4;
     int b = 5;
     printf("The sum of %d and %d is %d\n", a, b, a + b);
+    printf("Agrego este log para generar problemas de mergeo\n");
     return 0;
 }
